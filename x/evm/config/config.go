ec40d7d9f960144ba51b3835a15fb8b095034310
Uday Patil <udpatil@gmail.com>
2024-04-24 09:38:40 -0500

    Add sei-loadtest-chain evm chain ID mapping

--- x/evm/config/config.go
+++ x/evm/config/config.go
@@ -9,8 +9,9 @@ var ChainIDMapping = map[string]int64{
 	// pacific-1 chain ID == 0x531
 	"pacific-1": int64(1329),
 	// atlantic-2 chain ID == 0x530
-	"atlantic-2": int64(1328),
-	"arctic-1":   int64(713715),
+	"atlantic-2":         int64(1328),
+	"arctic-1":           int64(713715),
+	"sei-loadtest-chain": int64(112358),
 }
 
 func GetEVMChainID(cosmosChainID string) *big.Int {
